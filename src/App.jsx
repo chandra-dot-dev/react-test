c1734ec5d7ff994ac758fd26002396f45cb004f9
Chandra Kanth Reddy <chandrakanthreddy1231@gmail.com>
2025-12-01 12:45:51 +0530

    add files to git

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Learn react with Chaicode.com</h1>
+        <p>This is a sub heading.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
